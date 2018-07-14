21443ed32770708c67189795f10ec9ec93cfc26b
Karsten Bruns <karsten@bruns.me>
2018-07-15 00:42:52 +0200

    add vip model gen

--- Sources/Source.swift
+++ Sources/Source.swift
@@ -1,89 +1,61 @@
-import Foundation
 
-protocol Person {
-    var name: String { get }
-}
 
-struct User: Person {
-    let name: String
-    let homes: [Home]
-}
+enum EditProfileSceneModel: SceneModel {
 
-struct Home {
-    let admin: User
-    let address: String
-    let devices: [Device]
-}
-
-struct Address {
-    let street: String
-    let city: String
-}
-
-struct Device {
-    // sourcery: case_properties
-    enum Kind {
-        case light(status: Status<LightReading>)
-        case powerSwitch(status: Status<PowerSwitchReading>)
-        case blinds(status: Status<BlindsReading>)
-        case airConditioning(status: Status<AirConditioningReading>)
-    }
-
-    let id: Int
-    let name: String
-    let kind: Kind
-}
-
-// sourcery: case_properties
-enum Status<R> {
-    case connected(reading: R)
-    case disconnected
-}
-
-struct LightReading {
-    // sourcery: case_properties
-    enum State {
-        case on(brightness: Double)
-        case off
-    }
-
-    let state: State
-}
-
-struct PowerSwitchReading {
-    // sourcery: case_properties
-    enum State {
-        case on
-        case off
-    }
-
-    let state: State
-}
-
-struct BlindsReading {
-    // sourcery: case_properties
-    enum State {
-        case moving(targetPosition: Double, targetAngle: Double)
-        case stopped
-    }
-
-    let state: State
-    let currentPosition: Double
-    let currentAngle: Double
-}
-
-struct AirConditioningReading {
-    // sourcery: case_properties
-    enum State {
-        enum Mode {
-            case cooling
-            case heating
+    enum UserData {
+        struct Request { }
+        struct Response {
+            let user: User
         }
-
-        case on(targetTemperature: Double, mode: Mode)
-        case off
+        struct ViewModel {
+            let image: UIImage
+            let userName: String
+        }
     }
 
-    let state: State
-    let currentTemperature: Double
+    enum UserImageChange {
+        struct Request {
+            let newImage: UIImage
+        }
+        struct Response {
+            let uploadProgress: Double
+        }
+        struct ViewModel {
+            let message: String?
+        }
+    }
+
+    enum UserNameChange {
+        struct Request {
+            let newUserName: String
+        }
+        struct Response {
+            let containsInvalidCharacters: Bool
+            let isTooShort: Bool
+            let isAlreadyTaken: Bool
+        }
+        struct ViewModel {
+            let message: String?
+        }
+    }
+
+    enum Cancellation {
+        struct Request { }
+        struct Response {
+            let unsavedChanges: Bool
+        }
+        struct ViewModel {
+            let message: String?
+        }
+    }
+
+    enum Submit {
+        struct Request { }
+        struct Response {
+            let error: SomeError
+        }
+        struct ViewModel {
+            let message: String?
+        }
+    }
 }
--- Sources/Source2.swift
+++ Sources/Source2.swift
@@ -1,28 +1,5 @@
-//sourcery: mock
-protocol HomeControlProtocol {
-    func add(device: Device)
-    func remove(device: Device)
-    func absenceMode(enabled: Bool)
-}
+protocol SceneModel { }
 
-//sourcery: test
-class HomeControl: HomeControlProtocol {
+struct User { }
 
-    private(set) var home: Home
-    
-    init(home: Home) {
-        self.home = home
-    }
-
-    func add(device: Device) {
-        // Add device to home
-    }
-
-    func delete(device: Device) {
-        // Remove device from home
-    }
-
-    func absenceMode(enabled: Bool) {
-        // Enable/disable absence mode
-    }
-}
+struct SomeError { }
