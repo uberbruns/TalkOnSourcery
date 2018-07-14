4c5fed7186d931c605bf3ff9f276febffcb36c33
Karsten Bruns <karsten@bruns.me>
2018-07-14 20:36:25 +0200

    add iot case properties

--- Sources/IOTModel.swift
+++ Sources/IOTModel.swift
@@ -1,17 +1,12 @@
+import Foundation
+
 protocol Person {
-    let name: String
+    var name: String { get }
 }
 
 struct User: Person {
+    let name: String
     let homes: [Home]
-}
-
-// sourcery: case_properties
-enum Device {
-    case light(on: Bool)
-    case powerSwitch
-    case blinds
-    case heating
 }
 
 struct Home {
@@ -23,4 +18,72 @@ struct Home {
 struct Address {
     let street: String
     let city: String
+}
+
+struct Device {
+    // sourcery: case_properties
+    enum Kind {
+        case light(status: Status<LightReading>)
+        case powerSwitch(status: Status<PowerSwitchReading>)
+        case blinds(status: Status<BlindsReading>)
+        case airConditioning(status: Status<AirConditioningReading>)
+    }
+
+    let id: Int
+    let name: String
+    let kind: Kind
+}
+
+// sourcery: case_properties
+enum Status<R> {
+    case connected(reading: R)
+    case disconnected
+}
+
+struct LightReading {
+    // sourcery: case_properties
+    enum State {
+        case on(brightness: Double)
+        case off
+    }
+
+    let state: State
+}
+
+struct PowerSwitchReading {
+    // sourcery: case_properties
+    enum State {
+        case on
+        case off
+    }
+
+    let state: State
+}
+
+struct BlindsReading {
+    // sourcery: case_properties
+    enum State {
+        case moving(targetPosition: Double, targetAngle: Double)
+        case stopped
+    }
+
+    let state: State
+    let currentPosition: Double
+    let currentAngle: Double
+}
+
+struct AirConditioningReading {
+    // sourcery: case_properties
+    enum State {
+        enum Mode {
+            case cooling
+            case heating
+        }
+
+        case on(targetTemperature: Double, mode: Mode)
+        case off
+    }
+
+    let state: State
+    let currentTemperature: Double
 }
