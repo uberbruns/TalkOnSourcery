9e670c1536612377bb8d58fd98112e7dc122465e
Karsten Bruns <karsten@bruns.me>
2018-07-14 20:38:22 +0200

    add iot home project

---
+++ Projects/IOTHome/IOTHome/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  IOTHome
+//
+//  Created by Karsten Bruns on 14.07.18.
+//  Copyright © 2018 Karsten Bruns. All rights reserved.
+//
+
+import Foundation
+
+let device = Device(id: 123, name: "Light", kind: .light(status: .connected(reading: LightReading(state: .on(brightness: 0.5)))))
+
+if let brightness = device.kind.light?.status.connected?.reading.state.on?.brightness {
+  print(brightness)
+}
+
+if case .light(.connected(let reading)) = device.kind, case .on(let brightness) = reading.state {
+  print(brightness)
+}
+
+
