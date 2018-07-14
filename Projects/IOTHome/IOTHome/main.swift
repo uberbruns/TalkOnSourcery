954818444c5bc02beedfa8b3a2586c3f1873194a
Karsten Bruns <karsten@bruns.me>
2018-07-14 22:37:41 +0200

    modify xcode example

--- Projects/IOTHome/IOTHome/main.swift
+++ Projects/IOTHome/IOTHome/main.swift
@@ -10,12 +10,12 @@ import Foundation
 
 let device = Device(id: 123, name: "Light", kind: .light(status: .connected(reading: LightReading(state: .on(brightness: 0.5)))))
 
+
 if let brightness = device.kind.light?.status.connected?.reading.state.on?.brightness {
   print(brightness)
 }
 
+
 if case .light(.connected(let reading)) = device.kind, case .on(let brightness) = reading.state {
   print(brightness)
 }
-
-
