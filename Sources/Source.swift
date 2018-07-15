773eb93c0091e52463999fb5d4a9dad20435711a
Karsten Bruns <karsten@bruns.me>
2018-07-15 12:38:15 +0200

    add new service

--- Sources/Source.swift
+++ Sources/Source.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct GlobalEnvironment
     // sourcery:inline:Global.Environment
+    : TrackingServiceEnvironmentProtocol
     // sourcery:end
 {
     var locationManager: LocationManagerProtocol = LocationManager()
@@ -30,5 +31,34 @@ class TrackingService {
 // sourcery:inline:TrackingService.Environment.Protocol
 protocol TrackingServiceEnvironmentProtocol {
     var locationManager: LocationManagerProtocol { get }
+}
+// sourcery:end
+
+
+// sourcery:build_environment
+class SettingsService {
+
+    private struct Environment {
+        let database: DatabaseProtocol
+    }
+
+    // sourcery:inline:SettingsService.Environment.Properties
+    typealias EnvironmentProtocol = SettingsServiceEnvironmentProtocol
+    private let environment: EnvironmentProtocol
+    private var database: DatabaseProtocol { return environment.database }
+    // sourcery:end
+
+    init(environment: EnvironmentProtocol) {
+        self.environment = environment
+    }
+
+    func save() {
+        dump(database.path)
+    }
+}
+
+// sourcery:inline:SettingsService.Environment.Protocol
+protocol SettingsServiceEnvironmentProtocol {
+    var database: DatabaseProtocol { get }
 }
 // sourcery:end
