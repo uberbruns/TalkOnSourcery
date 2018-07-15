2b65bae050116ee93da2d6b233f6057304b33ae9
Karsten Bruns <karsten@bruns.me>
2018-07-15 11:24:47 +0200

    remove inlined code

--- Sources/Source.swift
+++ Sources/Source.swift
@@ -4,9 +4,6 @@ import Foundation
 
 struct GlobalEnvironment:
     // sourcery:inline:Global.Environment
-    AnyEnvironment,
-    SettingsServiceEnvironmentProtocol,
-    TrackingServiceEnvironmentProtocol {
     // sourcery:end
     var database: DatabaseProtocol = Database()
     var locationManager: LocationManagerProtocol = LocationManager()
@@ -23,16 +20,10 @@ class TrackingService {
     }
 
     // sourcery:inline:TrackingService.Environment.Properties
-    typealias EnvironmentProtocol = TrackingServiceEnvironmentProtocol
-
-    private let env: EnvironmentProtocol
-    private let locationManager: LocationManagerProtocol
     // sourcery:end
 
     init(env: EnvironmentProtocol, withLimit limit: Int) {
         // sourcery:inline:TrackingService.Environment.Init
-        self.env = env
-        self.locationManager = env.locationManager
         // sourcery:end
 
         let settingService = SettingsService(env: env)
@@ -41,9 +32,6 @@ class TrackingService {
 }
 
 // sourcery:inline:TrackingService.Environment.Protocol
-protocol TrackingServiceEnvironmentProtocol: SettingsServiceEnvironmentProtocol {
-    var locationManager: LocationManagerProtocol { get }
-}
 // sourcery:end
 
 
@@ -55,16 +43,10 @@ class SettingsService {
     }
 
     // sourcery:inline:SettingsService.Environment.Properties
-    typealias EnvironmentProtocol = SettingsServiceEnvironmentProtocol
-
-    private let env: EnvironmentProtocol
-    private let database: DatabaseProtocol
     // sourcery:end
 
     init(env: SettingsServiceEnvironmentProtocol) {
         // sourcery:inline:SettingsService.Environment.Init
-        self.env = env
-        self.database = env.database
         // sourcery:end
     }
 
@@ -75,7 +57,4 @@ class SettingsService {
 
 
 // sourcery:inline:SettingsService.Environment.Protocol
-protocol SettingsServiceEnvironmentProtocol {
-    var database: DatabaseProtocol { get }
-}
 // sourcery:end
