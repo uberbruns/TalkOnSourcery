bc9180ff8296d5a16bdf1dfd36f09b67c90a98a7
Karsten Bruns <karsten@bruns.me>
2018-07-15 11:31:46 +0200

    remove global env

--- Sources/Source.swift
+++ Sources/Source.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-// GLOBALE ENVIRONMENT
-
-struct GlobalEnvironment:
-    // sourcery:inline:Global.Environment
-    // sourcery:end
-    var database: DatabaseProtocol = Database()
-    var locationManager: LocationManagerProtocol = LocationManager()
-}
-
-
-
 // sourcery:build_environment
 class TrackingService {
 
@@ -28,29 +17,4 @@ class TrackingService {
 }
 
 // sourcery:inline:TrackingService.Environment.Protocol
-// sourcery:end
-
-
-// sourcery:build_environment
-class SettingsService {
-
-    private struct Environment {
-        let database: DatabaseProtocol
-    }
-
-    // sourcery:inline:SettingsService.Environment.Properties
-    // sourcery:end
-
-    init(env: SettingsServiceEnvironmentProtocol) {
-        // sourcery:inline:SettingsService.Environment.Init
-        // sourcery:end
-    }
-
-    func save() {
-        dump(database.path)
-    }
-}
-
-
-// sourcery:inline:SettingsService.Environment.Protocol
 // sourcery:end
