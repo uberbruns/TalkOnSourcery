9caaa590e1666d44e3ea8f9658bc38e2ac71d737
Karsten Bruns <karsten@bruns.me>
2018-07-15 12:36:09 +0200

    improve gloal env

--- Sources/Source.swift
+++ Sources/Source.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+struct GlobalEnvironment
+    // sourcery:inline:Global.Environment
+    // sourcery:end
+{
+    var locationManager: LocationManagerProtocol = LocationManager()
+}
+
+
 // sourcery:build_environment
 class TrackingService {
 
@@ -18,38 +26,9 @@ class TrackingService {
     }
 }
 
+
 // sourcery:inline:TrackingService.Environment.Protocol
 protocol TrackingServiceEnvironmentProtocol {
     var locationManager: LocationManagerProtocol { get }
-}
-// sourcery:end
-
-
-// sourcery:build_environment
-class SettingsService {
-
-    private struct Environment {
-        let database: DatabaseProtocol
-    }
-
-    // sourcery:inline:SettingsService.Environment.Properties
-    typealias EnvironmentProtocol = SettingsServiceEnvironmentProtocol
-    private let environment: EnvironmentProtocol
-    private var database: DatabaseProtocol { return environment.database }
-    // sourcery:end
-
-    init(environment: EnvironmentProtocol) {
-        self.environment = environment
-    }
-
-    func save() {
-        dump(database.path)
-    }
-}
-
-
-// sourcery:inline:SettingsService.Environment.Protocol
-protocol SettingsServiceEnvironmentProtocol {
-    var database: DatabaseProtocol { get }
 }
 // sourcery:end
